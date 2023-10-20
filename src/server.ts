5bd326cd33d86559f3e2fa27c7b12a59f8de4606
Dimas <dimasriat@gmail.com>
2023-10-21 00:24:36 +0700

    update

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ export function createServerApp() {
   const app = express();
 
   app.get("/", (req: Request, res: Response) => {
-    const TEXT = "dimski";
+    const TEXT = "nayski";
     console.log(printSomething(TEXT));
     console.log(printSomething(config.SECRET_MESSAGE));
 
