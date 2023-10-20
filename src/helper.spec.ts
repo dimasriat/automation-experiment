197109f0bf9f5e702458582c75862c1a11c0b5e0
Dimas <dimasriat@gmail.com>
2023-10-20 23:15:35 +0700

    add ci test

--- src/helper.spec.ts
+++ src/helper.spec.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from "@jest/globals";
 import { printSomething } from "./helper";
+import config from "./config";
 
 describe("printSomething", () => {
   it("prints text", () => {
@@ -17,5 +18,15 @@ describe("printSomething", () => {
         .filter((_, i) => i > 2)
         .join(" ")
     ).toBe(text);
+  });
+});
+
+describe("config", () => {
+  it("have secret messages", () => {
+    // arange
+    const msg = config.SECRET_MESSAGE;
+
+    // act
+    expect(msg).toBeTruthy();
   });
 });
